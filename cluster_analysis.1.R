a82fac2e734baf962fdb4afe835266a64f18325e
dacb <beck.dac@live.com>
2015-01-18 10:00:07 -0800

    updating image handling and parameter storage

--- cluster_analysis.1.R
+++ cluster_analysis.1.R
@@ -1,20 +1,20 @@
-dat <- list (
+env <- list (
+		study.title="5GB1",
 		file.sample_info="sample_info.xls",
 		file.rpkm="5G_rpkm.xls",
 		file.counts="5G_counts.xls",
-		reference.sample="FM23_TR3",
-		study.title="5GB1"
+		reference.sample="FM23_TR3"
 	)
 
-sample_info <- read.delim(dat[["file.sample_info"]], header=T, row.names=1, sep="\t")
+sample_info <- read.delim(env$file.sample_info, header=T, row.names=1, sep="\t")
 head(sample_info)
 names(sample_info)
 
-rpkm <- read.delim(dat[["file.rpkm"]], header=T, row.names=1, sep="\t")
+rpkm <- read.delim(env$file.rpkm, header=T, row.names=1, sep="\t")
 head(rpkm)
 names(rpkm)
 
-d <- read.delim(dat[["file.counts"]], header=T, row.names=1, sep="\t")
+d <- read.delim(env$file.counts, header=T, row.names=1, sep="\t")
 head(d)
 names(d)
 
@@ -32,10 +32,16 @@ rld <- rlog(dds)
 head(assay(rld))
 
 # normalize by reference sample log ratio
-res <- data.frame(assay(rld) - assay(rld)[,dat[["reference.sample"]]])
+res <- data.frame(assay(rld) - assay(rld)[,env$reference.sample])
 # remove reference sample from count matrix
-res <- res[, !names(res) %in% dat[["reference.sample"]]]
+res <- res[, !names(res) %in% env$reference.sample]
 
 head(res)
 
-save.image(file = "cluster_analysis.1.RData", compress=T)
+# update our environment object
+env$sample.info <- sample_info
+env$rpkm <- rpkm
+env$cnts.raw <- d
+env$cnts.norm <- res
+
+save(env, file = "cluster_analysis.1.RData", compress=T)
--- cluster_analysis.2.R
+++ cluster_analysis.2.R
@@ -2,14 +2,16 @@ library(flexclust)
 
 load("cluster_analysis.1.RData")
 
+env$clustSeed <- 8576
+
 #apply K-Means with various cluster number
 cls<-c(seq(10,40,by=5))
 #cls<-c(seq(4,40,by=2), seq(50,100,by=10))
 #cls<-c(seq(5,100,by=1))
-clustEnsemble=stepFlexclust(res,cls, nrep=3, save.data=TRUE, drop=FALSE, verbose=TRUE)
+env$cluster.ensemble <- stepFlexclust(env$cnts.norm, cls, nrep=3, save.data=TRUE, drop=FALSE, verbose=TRUE, seed=env$clustSeed)
 
 pdf("cluster_analysis.2.pdf")
-plot(clustEnsemble)
+plot(env$cluster.ensemble)
 dev.off()
 
-save.image("cluster_analysis.2.RData")
+save(env, file="cluster_analysis.2.RData")
