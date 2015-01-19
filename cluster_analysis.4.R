9403d260d51fde9d120a3460540ca3cfdf7a91bc
dacb <beck.dac@live.com>
2015-01-19 01:36:35 -0800

    by test and value indexing for flexclust compatability

--- cluster_analysis.4.R
+++ cluster_analysis.4.R
@@ -7,7 +7,6 @@ load("cluster_analysis.3.RData")
 
 # read meme data
 meme.data <- list();
-root.dir <- 'cluster_analysis.dir'
 for (i in 1:length(env$cluster.ensemble@k)) {
 	clusts <- clusters(env$cluster.ensemble[[i]])
 	meme.data.cluster <- list()
@@ -15,19 +14,18 @@ for (i in 1:length(env$cluster.ensemble@k)) {
 		clust <- clusts[clusts==j]
 
 		# setup pathes for input
-		dir <- paste(root.dir, paste("k_", env$cluster.ensemble[[i]]@k, ".dir/cluster_", j, ".dir", sep=""), sep="/")
+		dir <- paste(env$dir.output, paste("k_", env$cluster.ensemble[[i]]@k, ".dir/cluster_", j, ".dir", sep=""), sep="/")
 		meme_file <- paste(dir, "meme.txt", sep="/")
 		
 		# load the meme output file
-		print(meme_file)
 		if (file.exists(meme_file)) {
 			meme_text <- readLines(meme_file)
-			meme.data.cluster[[j]] <- memeParse(meme_text)
+			meme.data.cluster[[as.character(j)]] <- memeParse(meme_text)
 		} else {
 			warning(paste("meme.txt missing for k =", env$cluster.ensemble[[i]]@k, "cluster =", j))
 		}
 	}
-	meme.data[[i]] <- meme.data.cluster
+	meme.data[[as.character(i)]] <- meme.data.cluster
 }
 warnings()
 
