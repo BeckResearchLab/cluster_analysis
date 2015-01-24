e18bc5354e58059a16d200dbddf0c1519e10b892
dacb <dacb@uw.edu>
2015-01-24 15:12:36 -0800

    robust error handling for invalid training set cases

--- env.meme.data.R
+++ env.meme.data.R
@@ -15,24 +15,36 @@ env.meme.data.setup <- function() {
 			clust <- clusts[clusts==j]
 	
 			# setup pathes for input
-			dir <- paste(env$dir.output, paste("k_", env$cluster.ensemble[[i]]@k, ".dir/cluster_", j, ".dir", sep=""), sep="/")
+			dir <- dir.k.cluster(env$dir.output, env$cluster.ensemble[[i]]@k, j)
 			meme.file <- paste(dir, env$file.meme.txt, sep="/")
 
 			# setup the training set data frame to be validated in memeParse
 			training.set <- data.frame(length=env$genes$upstream.seqs[names(clust),"uplength"], row.names=names(clust))
 			# remove any NA (i.e. the gene had no upstream sequence because of an overlap)
 			training.set <- training.set[!is.na(training.set$length),"length", drop = F]
-			
-			# load the meme output file
-			meme.data.cluster[[as.character(j)]] <- memeParse(meme.file, training.set)
-			files.count <- files.count + 1
-			if (files.count %% 100 == 0) {
-				cat(paste("...processed", files.count, "files\n"))
+	
+			if (length(training.set$length) > 1) {	
+				# load the meme output file
+				meme.data.cluster[[as.character(j)]] <- memeParse(meme.file, training.set)
+				files.count <- files.count + 1
+				if (files.count %% 100 == 0) {
+					cat(paste("...processed", files.count, "files\n"))
+				}
+			} else {
+				if (file.exists(meme.file)) {
+					stop(
+						sprintf("no valid training set for k = %d and cluster = %d, but meme output file was present: %s", env$cluster.ensemble[[i]]@k, j, meme.file)
+					)
+				}
+				warning(
+					sprintf("no valid training set for k = %d and cluster = %d", env$cluster.ensemble[[i]]@k, j)
+				)
+				meme.data.cluster[[as.character(j)]] <- NA
 			}
 		}
 		meme.data[[as.character(env$cluster.ensemble@k[i])]] <- meme.data.cluster
 	}
 	warnings()
-	cat(paste("...processed", files.count, "files in total\n"))
+	cat(paste("...processed", files.count, "files total\n"))
 	return(meme.data)
 }
--- env.meme.sites.R
+++ env.meme.sites.R
@@ -7,25 +7,42 @@ env.meme.sites.setup <- function() {
 	for (i in 1:length(env$cluster.ensemble@k)) {
 		# lookup the cluster data and meme data for this k run
 		clusts <- clusters(env$cluster.ensemble[[i]])
-		cmemes <- env$meme.data[[i]]
+		cmemes <- env$meme.data[[as.character(env$cluster.ensemble@k[i])]]
+		# no meme data for this k?
+		if (length(cmemes) <= 0) {
+			stop(
+				sprintf("no valid meme data for k = %d", env$cluster.ensemble@k[i])
+			)
+		}
+
 		meme.sites.cluster <- list()
 		for (j in 1:env$cluster.ensemble@k[i]) {
 			# lookup the cluster specific ids and memes
 			clust <- clusts[clusts==j]
+	
+			# get the list of memes for this cluster	
 			memes <- cmemes[[j]]
+
+			# empty list of motifs which can happen if the training set is too small
+			if (is.na(memes) || length(memes) <= 0) {
+				warning(
+					sprintf("no valid motifs predicted for k = %d, cluster = %d", env$cluster.ensemble@k[i], j)
+				)
+				next
+			}
 
 			sites <- list()
 			mi <- 1
 			for (motif in memes) {
 				# new data frame from position table, fill in motif and with with rep
 				# compute xmin, xmax for easy ggplot viewing
-				uplen <- env$genes$upstream.seqs[as.character(motif$posns$gene),"uplength"]
+				uplen <- env$genes$upstream.seqs[as.character(motif$positions$gene),"uplength"]
 				df <- data.frame(
-					cbind(motif$posns),
-					motif=rep(mi, length(motif$posns$gene)),
-					width=rep(motif$width, length(motif$posns$gene)),
-					xmin=motif$posns$start-uplen,
-					xmax=motif$posns$start+motif$width-uplen
+					cbind(motif$positions),
+					motif=rep(mi, length(motif$positions$gene)),
+					width=rep(motif$width, length(motif$positions$gene)),
+					xmin=motif$positions$start-uplen,
+					xmax=motif$positions$start+motif$width-uplen
 				)
 				sites[[as.character(mi)]] <- df
 				mi <- mi + 1
