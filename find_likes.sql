8527c733c02fdfddfd32365829973fec9bc74c3f
dacb <dacb@uw.edu>
2015-02-03 19:59:49 -0800

    intermediate checking

--- find_likes.sql
+++ find_likes.sql
@@ -1,6 +1,7 @@
 SET @instance_pid = NULL;
 SET @instance_time = NULL;
 SET @session_id = NULL;
+SET @clusterLike = NULL;
 SET @myClusterLike = NULL;
 
 SELECT * FROM (
@@ -8,15 +9,20 @@ SELECT * FROM (
 SELECT * ,
 		IF(`instance.pid` = @instance_pid AND
 			`instance.time` = @instance_time AND
-			`session.id` = @session_id AND 
-			myClusterLike != @myClusterLike, 1, 0) AS liked,
+			`session.id` = @session_id,
+				IF(clusterLike != @clusterLike, "clusterLike",
+					IF(myClusterLike != @myClusterLike, "myClusterLike",
+						""
+					)
+				) AS liked_how,
 		@instance_pid := `instance.pid`,
 		@instance_time := `instance.time`,
 		@session_id := `session.id`,
+		@clusterLike := clusterLike
 		@myClusterLike := myClusterLike
 	FROM log
 	ORDER BY `instance.time`, `instance.pid`, `session.id`, `session.counter`
 
 ) AS ordered_log 
-	WHERE ordered_log.liked = 1
+	WHERE NOT(ISNULL(ordered_log.liked_how))
 ;
